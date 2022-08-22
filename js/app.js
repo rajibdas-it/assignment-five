685fc45080332c9f2279a7a80149f104af8b15b0
Rajib Das <rajibrad@gmail.com>
2022-08-22 12:07:06 +0600

    create array for sotre player name and apply condition

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+let palyersName = [];
 function addToList(clickedBtn) {
   const playerName = clickedBtn.parentNode.parentNode.children[0].innerText;
-  console.log(playerName);
+  if (palyersName.length <= 4) {
+    palyersName.push(playerName);
+  } else {
+    console.log("you cannot added more than 4");
+  }
 }
+
+console.log(palyersName);
