ac1b6647408736981a112aa3d27031ebc0506516
Rajib Das <rajibrad@gmail.com>
2022-08-22 13:26:42 +0600

    name added from array into list

--- js/app.js
+++ js/app.js
@@ -3,9 +3,25 @@ function addToList(clickedBtn) {
   const playerName = clickedBtn.parentNode.parentNode.children[0].innerText;
   if (palyersName.length <= 4) {
     palyersName.push(playerName);
+    clickedBtn.disabled = true;
+    // clickedBtn.setAttribute("disable", true);
   } else {
-    console.log("you cannot added more than 4");
+    alert("you cannot added more than 4");
   }
-}
+  const olList = document.getElementById("player-list");
+  for (i = 0; i < palyersName.length; i++) {
+    player = palyersName[i];
+    const li = document.createElement("li");
+    // li.innerText = "";
+    li.innerText = `${i + 1}. ${player}`;
+    olList.appendChild(li);
 
-console.log(palyersName);
+    console.log(player);
+  }
+  //   const li = document.createElement("li");
+  //   li.innerText = playerName;
+  //   olList.appendChild(li);
+  //   li.innerText = "";
+
+  console.log(palyersName);
+}
